ded72c7832ecf9148695365e5099b3b8d862eb54
Ravikeerthy <ravikeerthy1807@gmail.com>
2024-06-23 13:08:43 +0400

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.getElementById("mathFun").addEventListener("click", () => {
-    number();
-    
-  });
-  async function number() {
+  number();
+});
+async function number() {
+  try {
     let getdata = await fetch("http://numbersapi.com/random/trivia");
     let resdata = await getdata.text();
     console.log(resdata);
     document.getElementById("displayDiv").innerText = resdata;
-  
-    
+  } catch (error) {
+    console.log("Error Fetching Data", error);
+    document.getElementById("displayDiv").innerText = "Error Fetching Data";
   }
-  
+}
